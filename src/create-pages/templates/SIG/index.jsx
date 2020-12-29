0244709fcac62fac4cbe5eb075b4ccc2d333bfe6
昌维 <867597730@qq.com>
2020-12-29 16:55:28 +0800

    reuse component Container in SIG template

--- src/create-pages/templates/SIG/index.jsx
+++ src/create-pages/templates/SIG/index.jsx
@@ -23,7 +23,7 @@ export default function SIG({ data, pageContext}) {
       </Banner>
   
       <div className={styles.wrapper}>
-        <Container className="container">
+        <Container className={styles.container}>
           <div className={styles.summary}>
             The TiDB project is organized primarily into Special Interest Groups (SIG). Each SIG is comprised of members from multiple companies and organizations, with a common purpose of advancing the project with respect to a specific topic, such as Parser/Expression/Planner. Our goal is to enable a distributed decision structure and code ownership, as well as providing focused forums for getting work done, making decisions, and onboarding new contributors. Every identifiable subpart of the project (e.g., github org, repository, subdirectory, API, test, issue, PR) is intended to be owned by some SIG. For more details about sig governance, you can read this doc sig governance. And if you are new to tidb, and want to find a sig to start, this contribution map for sig may be helpful.
           </div>

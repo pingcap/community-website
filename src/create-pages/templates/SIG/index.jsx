ae66ad7c3b0a47ee3079d4396ad5306c87ede340
昌维 <867597730@qq.com>
2020-12-31 10:57:29 +0800

    add Popular Groups section in SIG list page

--- src/create-pages/templates/SIG/index.jsx
+++ src/create-pages/templates/SIG/index.jsx
@@ -38,16 +38,16 @@ export default function SIG({ data, pageContext}) {
             The TiDB project is organized primarily into Special Interest Groups (SIG). Each SIG is comprised of members from multiple companies and organizations, with a common purpose of advancing the project with respect to a specific topic, such as Parser/Expression/Planner. Our goal is to enable a distributed decision structure and code ownership, as well as providing focused forums for getting work done, making decisions, and onboarding new contributors. Every identifiable subpart of the project (e.g., github org, repository, subdirectory, API, test, issue, PR) is intended to be owned by some SIG. For more details about sig governance, you can read this doc sig governance. And if you are new to tidb, and want to find a sig to start, this contribution map for sig may be helpful.
           </div>
           <Row className={styles.list}>
-            <Col span={24}>
+            <Col sm={24} md={16}>
               <Section name="All Special Interest Groups">
                 {items.map(item => <SIGItem {...item} />)}
               </Section>
             </Col>
-            {/*<div className="column is-4">*/}
-            {/*  <Section name="Popular groups">*/}
-            {/*    Popular groups*/}
-            {/*  </Section>*/}
-            {/*</div>*/}
+            <Col sm={24} md={8}>
+              <Section name="Popular groups">
+                Popular groups
+              </Section>
+            </Col>
           </Row>
         </Container>
       </div>

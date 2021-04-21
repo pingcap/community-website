bd80f7cfc36f864d306215ab805d932f389050af
Nick Qi <wei.qi.nick@gmail.com>
2021-04-21 19:46:25 +1200

    trigger build

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
-const createHome = require('./src/create-pages/createHome')
-const createSIG = require('./src/create-pages/createSIG')
-const createPeople = require('./src/create-pages/createPeople')
-const createRanking = require('./src/create-pages/createRanking')
+const createHome = require("./src/create-pages/createHome");
+const createSIG = require("./src/create-pages/createSIG");
+const createPeople = require("./src/create-pages/createPeople");
+const createRanking = require("./src/create-pages/createRanking");
 const createIntlPages = require("./src/create-pages/intl");
 
 exports.onCreateWebpackConfig = ({ stage, actions }) => {
@@ -13,27 +13,27 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
         "node_modules",
       ],
     },
-  })
-}
+  });
+};
 
 exports.createPages = async ({ actions, graphql }) => {
-  const { createPage, createRedirect } = actions
-  
+  const { createPage, createRedirect } = actions;
+
   // createRedirect({
   //   fromPath: '/download/community/',
   //   toPath: '/download',
   //   redirectInBrowser: true,
   //   isPermanent: true,
   // })
-  
+
   await Promise.all([
     createHome({ graphql, createPage, createRedirect }),
     createPeople({ graphql, createPage, createRedirect }),
     createSIG({ graphql, createPage, createRedirect }),
     createRanking({ graphql, createPage, createRedirect }),
-  ])
-}
+  ]);
+};
 
 exports.onCreatePage = ({ page, actions }) => {
-  createIntlPages({ page, actions })
-}
+  createIntlPages({ page, actions });
+};

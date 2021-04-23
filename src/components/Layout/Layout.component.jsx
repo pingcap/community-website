5969a4385f5161f4f81dc9873f00dd8034c15eff
Nick Qi <wei.qi.nick@gmail.com>
2021-04-23 18:21:46 +1200

    CPT-82 feat: use the community logo

--- src/components/Layout/Layout.component.jsx
+++ src/components/Layout/Layout.component.jsx
@@ -3,31 +3,21 @@ import { Footer, Header, utils } from '@tidb-community/ui';
 import { Location } from '@reach/router';
 import { getData } from '@tidb-community/datasource';
 import { navigate } from 'gatsby';
-import { useDebounce } from 'ahooks';
 import { useIntl } from 'react-intl';
 
-import helper from '../../helper';
-import styles from './layout.module.scss';
+import communityLogo from '../../../images/community-logo.svg';
 
 export default function Layout({ children, ...rest }) {
   const intl = useIntl();
   const { locale } = intl;
-
-  const transparentDebounced = useDebounce(false, { wait: 100 });
-  const logoImageUrl = helper.getLogoByLocale(locale, transparentDebounced);
 
   const { header: headerData, footer: footerData } = getData({
     domain: 'contributor.tidb.io',
     locale,
   }).nav;
 
-  const Logo = () => (
-    <>
-      <div className={styles.logo}>
-        <img src={logoImageUrl} alt="TiDB DevGroup" />
-      </div>
-    </>
-  );
+  const title = 'TiDB Community';
+  const logo = <img alt={title} src={communityLogo} />;
 
   const onNavClick = (link) => {
     if (link.startsWith('http')) {
@@ -42,20 +32,20 @@ export default function Layout({ children, ...rest }) {
   };
 
   const headerProps = ({ location }) => ({
-    logo: <Logo />,
-    navItems: headerData.navItems,
-    title: '',
-    currentNav: utils.header.getCurrentNav(headerData.navItems, location.pathname),
+    logo,
+    title,
     onNavClick,
     onTitleClick,
+    navItems: headerData.navItems,
+    currentNav: utils.header.getCurrentNav(headerData.navItems, location.pathname),
   });
 
   const footerProps = {
-    logo: <Logo />,
-    navItems: footerData.navItems,
-    title: '',
-    icons: footerData.icons,
+    logo,
+    title,
     onNavClick,
+    icons: footerData.icons,
+    navItems: footerData.navItems,
   };
 
   return (

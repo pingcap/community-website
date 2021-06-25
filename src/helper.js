8c6763dfa82102cd8a20e7b05db39c6da8429916
Nick Qi <wei.qi.nick@gmail.com>
2021-06-25 21:28:32 +1200

    CP-37 feat: update PeopleDetail with the description metadata

--- src/helper.js
+++ src/helper.js
@@ -1,20 +1,24 @@
 module.exports = {
   convertToUpperCamelCase: (str) => {
     switch (typeof str) {
-      case "number":
-      case "boolean": return '' + str
-      case "string": {
+      case 'number':
+      case 'boolean':
+        return '' + str;
+      case 'string': {
         const reg = /[_-]([a-z])/g;
-        str = str.substring(0,1).toUpperCase() + str.substring(1)
-        return str.replace(reg, (a, b) => ' ' + b.toUpperCase())
+        str = str.substring(0, 1).toUpperCase() + str.substring(1);
+        return str.replace(reg, (a, b) => ' ' + b.toUpperCase());
       }
-      default: return ''
+      default:
+        return '';
     }
   },
-  
+
   getLogoByLocale: (locale, transparent) => {
-    const logoImageName = locale === 'en' ? `logo-with-text` : `logo-with-text-${locale}`
-    const logoImageNameTransparent = locale === 'en' ? `logo-with-white-text` : `logo-with-white-text-${locale}`
-    return transparent ? `https://download.pingcap.com/community-devgroup/${logoImageNameTransparent}.svg` : `https://download.pingcap.com/community-devgroup/${logoImageName}.svg`
-  }
-}
+    const logoImageName = locale === 'en' ? `logo-with-text` : `logo-with-text-${locale}`;
+    const logoImageNameTransparent = locale === 'en' ? `logo-with-white-text` : `logo-with-white-text-${locale}`;
+    return transparent
+      ? `https://download.pingcap.com/community-devgroup/${logoImageNameTransparent}.svg`
+      : `https://download.pingcap.com/community-devgroup/${logoImageName}.svg`;
+  },
+};

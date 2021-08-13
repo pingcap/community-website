19f9aea55e2f6319dfdd85cd37343fd2cc04bc3a
Nick Qi <wei.qi.nick@gmail.com>
2021-08-13 15:59:21 +1200

    CP-129 feat: track page view events by using the tracking script

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,12 @@
 import 'antd/dist/antd.css';
 import 'src/styles/globals.scss';
 
+import { logPageView } from '@tidb-community/tracking-script';
+
 export { wrapPageElement } from 'src/create-pages/wrapPage';
+
+export const onRouteUpdate = () => {
+  if (process.env.NODE_ENV === 'production') {
+    logPageView();
+  }
+};
